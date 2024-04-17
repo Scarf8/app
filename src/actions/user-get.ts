07d13f4c04565aa1ec40415bfedbcb7187ab5b99
temattcha <mateus.somos@gmail.com>
2024-04-16 21:14:03 -0300

    dump de gatinho

---
+++ src/actions/user-get.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import apiError from '@/functions/api-error';
+import login from './login';
+import { cookies } from 'next/headers';
+import userJson from '../app/json-data/user.json';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  isActive: boolean;
+};
+
+export default async function userGet() {
+  try {
+    // const url = 'http://localhost:5001/api/user';
+
+    // const response = await fetch(url, {
+    //   method: 'GET',
+    //   headers: {
+    //     Authorization: 'Bearer',
+    //   },
+    //   next: {
+    //     revalidate: 60,
+    //   },
+    // });
+    // if (!response.ok) throw new Error('Erro ao pegar o usuário.');
+    // const data = (await response.json()) as User;
+    const data = userJson;
+    return { data, ok: true, error: '' };
+  } catch (error: unknown) {
+    return apiError(error);
+  }
+}
